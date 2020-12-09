13691ce38b9e195d4bb81521bb14fade0bb2392e
kobihall <50158866+kobihall@users.noreply.github.com>
2020-12-09 15:42:16 -0800

    Create a test post processing effect shader

---
+++ Assets/PostProcessEffectScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostProcessEffectScript : MonoBehaviour
+{
+	public Material mat;
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+    	//src is the rendered scene input
+    	Graphics.Blit(src, dest, mat);
+    }
+}
