55941b59c84c63af5b80cfcfa960c823b1cf63eb
kobihall <50158866+kobihall@users.noreply.github.com>
2020-12-11 03:47:14 -0800

    Organization
    grouped important variables into structs, created a sphere struct, code to manage a scene and loop through multiple spheres. Organized functions in the script

--- Assets/RayTracingMaster.cs
+++ Assets/RayTracingMaster.cs
@@ -10,10 +10,108 @@ public class RayTracingMaster : MonoBehaviour{
     private Material _addMaterial;
     public Light DirectionalLight;
 
+    struct Sphere
+    {
+        public Vector3 position;
+        public float radius;
+        public Vector3 albedo;
+        public Vector3 specular;
+    }
 
-    private void OnRenderImage(RenderTexture source, RenderTexture destination){
-        Render(destination);
-        SetShaderParameters();
+    public Vector2 SphereRadius = new Vector2(3.0f, 8.0f);
+    public uint SpheresMax = 100;
+    public float SpherePlacementRadius = 100.0f;
+    private ComputeBuffer _sphereBuffer;
+
+    private Camera _camera;
+
+    private void Awake(){
+        _camera = GetComponent<Camera>();
+    }
+
+    private void OnEnable()
+    {
+        _currentSample = 0;
+        SetUpScene();
+    }
+
+    private void OnDisable()
+    {
+        if (_sphereBuffer != null)
+            _sphereBuffer.Release();
+    }
+
+    private void Update(){
+        //NOTE: this should also update along with the directional light at some point (it will reduce anti-aliasing noise)
+        if (transform.hasChanged)
+        {
+            _currentSample = 0;
+            transform.hasChanged = false;
+        }
+    }
+
+    private void SetUpScene()
+    {
+        List<Sphere> spheres = new List<Sphere>();
+
+        // Add a number of random spheres
+        for (int i = 0; i < SpheresMax; i++)
+        {
+            Sphere sphere = new Sphere();
+
+            // Radius and radius
+            sphere.radius = SphereRadius.x + Random.value * (SphereRadius.y - SphereRadius.x);
+            Vector2 randomPos = Random.insideUnitCircle * SpherePlacementRadius;
+            sphere.position = new Vector3(randomPos.x, sphere.radius, randomPos.y);
+
+            // Reject spheres that are intersecting others
+            foreach (Sphere other in spheres)
+            {
+                float minDist = sphere.radius + other.radius;
+                if (Vector3.SqrMagnitude(sphere.position - other.position) < minDist * minDist)
+                    goto SkipSphere;
+            }
+
+            // Albedo and specular color
+            Color color = Random.ColorHSV();
+            bool metal = Random.value < 0.5f;
+            sphere.albedo = metal ? Vector3.zero : new Vector3(color.r, color.g, color.b);
+            sphere.specular = metal ? new Vector3(color.r, color.g, color.b) : Vector3.one * 0.04f;
+
+            // Add the sphere to the list
+            spheres.Add(sphere);
+
+        SkipSphere:
+            continue;
+        }
+
+        // Assign to compute buffer
+        _sphereBuffer = new ComputeBuffer(spheres.Count, 40);
+        _sphereBuffer.SetData(spheres);
+    }
+
+    private void SetShaderParameters(){
+        RayTracingShader.SetMatrix("_CameraToWorld", _camera.cameraToWorldMatrix);
+        RayTracingShader.SetMatrix("_CameraInverseProjection", _camera.projectionMatrix.inverse);
+        RayTracingShader.SetTexture(0, "_SkyboxTexture", SkyboxTexture);
+        RayTracingShader.SetVector("_PixelOffset", new Vector2(Random.value, Random.value));
+        Vector3 l = DirectionalLight.transform.forward;
+        RayTracingShader.SetVector("_DirectionalLight", new Vector4(l.x, l.y, l.z, DirectionalLight.intensity));
+        RayTracingShader.SetBuffer(0, "_Spheres", _sphereBuffer);
+    }
+
+    private void InitRenderTexture(){
+        if (_target == null || _target.width != Screen.width || _target.height != Screen.height){
+            // Release render texture if we already have one
+            if (_target != null)
+                _target.Release();
+            // Get a render target for Ray Tracing
+            _target = new RenderTexture(Screen.width, Screen.height, 0,
+                RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
+            _target.enableRandomWrite = true;
+            _target.Create();
+            Update();
+        }
     }
 
     private void Render(RenderTexture destination){
@@ -32,40 +130,8 @@ public class RayTracingMaster : MonoBehaviour{
         _currentSample++;
     }
 
-    private void InitRenderTexture(){
-        if (_target == null || _target.width != Screen.width || _target.height != Screen.height){
-            // Release render texture if we already have one
-            if (_target != null)
-                _target.Release();
-            // Get a render target for Ray Tracing
-            _target = new RenderTexture(Screen.width, Screen.height, 0,
-                RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
-            _target.enableRandomWrite = true;
-            _target.Create();
-            Update();
-        }
-    }
-
-    private void Update(){
-        //NOTE: this should also update along with the directional light at some point (it will reduce anti-aliasing noise)
-        if (transform.hasChanged)
-        {
-            _currentSample = 0;
-            transform.hasChanged = false;
-        }
-    }
-
-    private Camera _camera;
-
-    private void Awake(){
-        _camera = GetComponent<Camera>();
-    }
-    private void SetShaderParameters(){
-        RayTracingShader.SetMatrix("_CameraToWorld", _camera.cameraToWorldMatrix);
-        RayTracingShader.SetMatrix("_CameraInverseProjection", _camera.projectionMatrix.inverse);
-        RayTracingShader.SetTexture(0, "_SkyboxTexture", SkyboxTexture);
-        RayTracingShader.SetVector("_PixelOffset", new Vector2(Random.value, Random.value));
-        Vector3 l = DirectionalLight.transform.forward;
-        RayTracingShader.SetVector("_DirectionalLight", new Vector4(l.x, l.y, l.z, DirectionalLight.intensity));
+    private void OnRenderImage(RenderTexture source, RenderTexture destination){
+        Render(destination);
+        SetShaderParameters();
     }
 }
